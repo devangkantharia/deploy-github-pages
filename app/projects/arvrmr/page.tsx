02e866d76b76c516c9ae4cf387cc2ff271c56b18
devangkantharia <devangkantharia@gmail.com>
2025-11-08 22:42:48 +0000

    testing img app static

--- app/projects/arvrmr/page.tsx
+++ app/projects/arvrmr/page.tsx
@@ -1,7 +1,17 @@
+import Image from 'next/image'
+import ProfileImage from '../../images/arvrmr.jpg'
+
 export default function ArVrMrPage() {
   return (
     <main>
       <div>AR/VR/MR Page</div>
+      <Image
+        src={ProfileImage}
+        alt="Picture of the author"
+      // width and height are now automatically provided
+      // blurDataURL="data:..." automatically provided
+      // placeholder="blur" // Optional blur-up while loading
+      />
     </main>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,11 @@ const nextConfig = {
 
   // Set the base path to the repository name for GitHub Pages.
   basePath: process.env.PAGES_BASE_PATH || '',
+
+  // Disable image optimization for static exports.
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
